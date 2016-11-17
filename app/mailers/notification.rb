2961102d5febeb0b16d659008e112610103d989b
Gonzalo Nahuel <gonzalo.nahuel.dipierro@gmail.com>
2016-11-17 13:24:25 -0300

    Algunas mejores mas para los emails

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -49,7 +49,7 @@ JobVacancy::App.mailer :notification do
            :email => job_application.job_offer.owner.email
     content_type :plain
     via :sendmail
-    render 'notification/contant_info_email'
+    render 'notification/contact_info_email'
   end
 
   email :offerer_info_email do | job_application |
@@ -60,7 +60,7 @@ JobVacancy::App.mailer :notification do
            :applicant_email => job_application.applicant_email
     content_type :plain
     via :sendmail 
-    render 'notification/contant_info_email'
+    render 'notification/offerer_info_email'
   end
 
 end
